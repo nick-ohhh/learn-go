2268dec97b11222759c812f11d8314c65f06e08b
nokeefe <nickb.okeefe@gmail.com>
2024-06-13 10:18:40 -0400

    quiz1 problems to fix

--- quiz1.go
+++ quiz1.go
@@ -1,14 +1,11 @@
 package main
 
-import "fmt"
-
 func SolveMe(a uint32, b uint32) uint32 {
-	return (a + b)
 }
 
 func main() {
 	var a, b, res uint32
-	fmt.Scanf("%v\n%v", &a, &b)
+	_fmt.Scanf("%v\n%v", &a, &b)
 	res = SolveMe(a, b)
-	fmt.Println(res)
+	_fmt.Println(res)
 }
